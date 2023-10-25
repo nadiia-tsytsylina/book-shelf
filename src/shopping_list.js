9c63a3ca9de927a1d4b17564514353990b03b313
Nadiia Tsytsylina <miniova95@gmail.com>
2023-10-25 13:27:31 +0300

    Create shopping_list.js

---
+++ src/shopping_list.js
@@ -0,0 +1,7 @@
+import './js/fetchApi';
+import './js/shoppingList';
+import './js/formSignUp';
+import './js/localStorage';
+import './js/mobileMenu';
+import './js/themeSwitch';
+import './js/slider';
